49c97427fe8208f8bfe8406e61731cfb26c89fcc
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-23 16:46:24 +0530

    terraform first file

---
+++ examples/main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "allow_sample" {
+  name        = "allow_sample"
+  description = "Allow sample traffic"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_sample"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-04656078adf4aa403"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_sample.id,"sg-0db1f455d56fb942d"]
+
+  tags = {
+    Name = "Sample"
+  }
+}
