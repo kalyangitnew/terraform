8603e00c5b398bd226390d958cb56fe320061237
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-23 20:17:50 +0530

    terraform first file

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -3,10 +3,16 @@ resource "aws_spot_instance_request" "cheap_worker" {
   ami           = data.aws_ami.ami.id
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-0db1f455d56fb942d"]
+  wait_for_fulfillment = true
 
   tags = {
-    Name = element(var.components,count.index )
+    Name = element(var.components,count.index)
   }
+}
+resource "aws_ec2_tag" "tags" {
+  key         = element("aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index")
+  resource_id = "Name"
+  value       = "element(var.components,count.index)"
 }
 data "aws_ami" "ami" {
   most_recent = true
