04ae1124b7ccf82c7e23dfaa98fec6b91677cec1
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-23 17:45:15 +0530

    terraform first file

--- examples/ec2/main.tf
+++ examples/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-04656078adf4aa403"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_sample.id,"sg-0db1f455d56fb942d"]
+  vpc_security_group_ids = ["sg-0db1f455d56fb942d"]
 
   tags = {
     Name = "Sample"
