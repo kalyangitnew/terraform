c012973956344b31332fd5bca7f6c2ec1f7a0c5a
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-23 18:09:20 +0530

    terraform first file

--- examples/ec2/main.tf
+++ examples/ec2/main.tf
@@ -1,11 +1,43 @@
 resource "aws_instance" "sample" {
-  ami           = "ami-04656078adf4aa403"
-  instance_type = "t2.micro"
+  count                  = var.env == "prod" ? 1 : 0
+  ami                    = data.aws_ami.example.id
+  instance_type          = var.instance_type == "" ? "t3.micro" : var.instance_type
   vpc_security_group_ids = [var.SGID]
 
   tags = {
-    Name = "sample"
+    Name = local.NAME
   }
 }
-variable "SGID" {
+
+resource "null_resource" "sample" {
+  #  triggers = {
+  #    abc = aws_instance.sample.*.private_ip
+  #  }
+  provisioner "remote-exec" {
+    connection {
+      host     = aws_instance.sample.*.public_ip[0]
+      user     = "ubuntu"
+      password = "DevOps321"
+    }
+
+    inline = [
+      "echo Hello"
+    ]
+  }
+}
+
+variable "SGID" {}
+variable "name" {}
+
+variable "instance_type" {}
+variable "env" {}
+
+data "aws_ami" "example" {
+  most_recent = true
+  name_regex  = "^Ubuntu*"
+  owners      = ["973714476881"]
+}
+
+locals {
+  NAME = "${var.env}-instance"
 }
--- examples/sg/main.tf
+++ examples/sg/main.tf
@@ -2,29 +2,39 @@ resource "aws_security_group" "allow_sample" {
   name        = "allow_sample"
   description = "Allow sample traffic"
 
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = []
-    self = false
-  }
+  ingress = [
+    {
+      description      = "TLS from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-    self = false
-  }
+  egress = [
+    {
+      description      = "egress"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
 
   tags = {
     Name = "allow_sample"
   }
 }
+
 output "SGID" {
-  value = "aws_security_group.allow_sample.id"
+  value = aws_security_group.allow_sample.id
 }
