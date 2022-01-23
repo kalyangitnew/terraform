6ab1c5916021ec7e231bd66aa388ed788bcfcb43
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-23 20:36:23 +0530

    terraform first file

--- roboshop/main.tf
+++ roboshop/main.tf
@@ -1,18 +1,17 @@
 resource "aws_spot_instance_request" "cheap_worker" {
-  count         = length(var.components)
-  ami           = data.aws_ami.ami.id
-  instance_type = "t2.micro"
+  count                  = length(var.components)
+  ami                    = data.aws_ami.ami.id
+  instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0db1f455d56fb942d"]
-  wait_for_fulfillment = true
-
+  wait_for_fulfillment   = true
   tags = {
     Name = element(var.components, count.index)
   }
 }
 resource "aws_ec2_tag" "tags" {
   count       = length(var.components)
-  key         = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
-  resource_id = "Name"
+  resource_id = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
+  key         = "Name"
   value       = element(var.components, count.index)
 }
 data "aws_ami" "ami" {
@@ -20,9 +19,11 @@ data "aws_ami" "ami" {
   name_regex  = "^Cent*"
   owners      = ["973714476881"]
 }
+
 variable "components" {
   default = ["frontend", "mongodb", "catalogue", "cart", "user", "redis", "mysql", "shipping", "rabbitmq", "payment"]
 }
+
 provider "aws" {
   region = "us-east-1"
 }
